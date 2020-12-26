8c68578e27d76033eda542e4d8077624a064b4e8
Lyriraki <Luckyarif21@gmail.com>
2020-12-26 16:43:18 +0700

    try prettier

--- pages/mbuh.jsx
+++ pages/mbuh.jsx
@@ -1,22 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 export default function About() {
   return (
     <Layout
-    mainTitle="My About is News"
-    footer={`Copyright ${new Date().getFullYear()}`}
-    className={styles.container}>
+      mainTitle="My About is News"
+      footer={`Copyright ${new Date().getFullYear()}`}
+      className={styles.container}
+    >
       <Head>
         <title>Dasar NextJS-Mbuh</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="My SEO React App With Next JS"/>
-        <meta name="keywords" content="Next React SEO"/>
-        <meta name="author" content="Lyriraki"/>
+        <meta name="description" content="My SEO React App With Next JS" />
+        <meta name="keywords" content="Next React SEO" />
+        <meta name="author" content="Lyriraki" />
       </Head>
-      
+
       <main className={styles.main}>
         <h1 className={styles.title}>Mbuh</h1>
       </main>
